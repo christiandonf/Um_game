3217df36c07a88376ca09d2d69a98e75e2bcd484
Christian <87277467+christiandonf@users.noreply.github.com>
2024-12-12 19:12:51 -0300

    Aula correção de Bugs
    Aqui vimos alex que é muito importante você se concentrar em identar o código pois se não fizer a identação corretamente o interpretador do gdscript vai te retornar um erro ai basta vocẽ identificar no identificador qual o motivo do erro dele só lembrando que fica mais difícil de entender quando seria uma função condicional dentro de uma função primária pois ai o interpretador não consegue responder direto ao erro de indentação

--- um game/AnimatedSprite.gd
+++ um game/AnimatedSprite.gd
@@ -1,24 +1,18 @@
 extends AnimatedSprite
 
-const SPEED := 400
 # Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+const SPEED = 400
+var cocacola = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-	
-	
-	func _process(delta):
-var velocity = Input.get_vector("move_left","move_right","move_up","move_down")
-
-if velocity.length() > 0:
-	velocity = velocity.normalized() * SPEED
-	
-	position += velocity * delta
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	var velocity = Input.get_vector("move_left","move_right","move_up","move_down")
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * SPEED * cocacola
+
+	position += velocity * delta 
