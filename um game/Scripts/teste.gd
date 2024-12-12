0d3610cb26e14565f004a5e0b9fca0404f8ab72e
Christian <87277467+christiandonf@users.noreply.github.com>
2024-12-11 23:37:30 -0300

    mudança para o alex ver
    uma mensagem bem legal que o alex deve concordar

--- um game/Scripts/teste.gd
+++ um game/Scripts/teste.gd
@@ -25,3 +25,5 @@
 
 
 #teste
+
+#essa é uma mensagem para o bundão do alex que pode não ter medo de fantasmas ou coisas parecidas mas tem muito medo de ter a bunda comida por tais fantasmas kskskskskskskkssk
