2798073050e8cf25c54eee47ffbe18e96cc7339f
Christian <87277467+christiandonf@users.noreply.github.com>
2024-12-14 14:16:38 -0300

    Pequenas mudanças de projeto
    nessa aula aprendemos o que são tipos de variáveis e quais os beneficios e os cuidados que temos que ter com cada variável que criamos, o intuito dessa aula alex é para você ter o conhecimento e entendimento sobre o que você está manipulando na programação, alem disso aprendemos também o que  são os erros a ler esses erros e tentar identificar o que erramos  para fazer a correção, também aprendemos sobre a organização do código e o quanto é importante para o funcionamento do GDscript mas isso não é tudo, organização é importante para tudo ainda mais quando seu projeto ficar ainda mais complexo pelo simples fato dele ficar maior

--- um game/Scripts/AnimatedSprite.gd
+++ um game/Scripts/AnimatedSprite.gd
@@ -1,18 +1,24 @@
 extends AnimatedSprite
 
 # Declare member variables here. Examples:
-const SPEED = 400
-var cocacola = 1
+const SPEED : int = 400
+export var cocacola : float = 1
+export var jogo : bool = false
+var vida : int = 15
+
+func _gethealth():
+	var getvida = vida
+	return getvida
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	vida = 20.1
+	print(_gethealth())
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var velocity = Input.get_vector("move_left","move_right","move_up","move_down")
-
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * SPEED * cocacola
-
-	position += velocity * delta 
+	position += velocity * delta
+	
---
+++ um game/Scripts/Area de informações.gd
@@ -0,0 +1,11 @@
+#Desafio 
+#Criar um personagem 2D que se move pela tela -CONCLUIDO-
+#Criar um Item coletável que o personagem possa interagir -NÃO CONCLUIDO-
+#Ao personagem Interagir aumentar a velocidade de movimento do mesmo -NÃO CONCLUIDO-
+
+#Variável do tipo INT (Numeros Racionais) valores em números inteiros: ...-5, -4, -3, -2 , -1, 0, 1, 2, 3, 4, 5...
+#Variável do tipo FLOAT (Números Irracionais) valores em números com vírgula: ...-1.3, -1.2, -1.1, -1, 0, 1, 1.1, 1.2, 1.3...
+#Variável do tipo STRING (Texto) valores em texto: "pode se escrever qualquer coisa 123"
+#Variável do tipo CHAR (letra) valor em letra: "a", "B", "@"...
+#Variável do tipo BOOL (binário) valor somente -Verdadeiro (true) ou Falso (false)-
+
