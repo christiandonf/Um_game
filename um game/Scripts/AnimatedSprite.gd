a77f4239b26663e68aca2a660d0c30daf31d2ee6
AlexTheOutsider68 <alexandre68borges@gmail.com>
2024-12-27 01:58:26 -0300

    mais um pequeno progresso
    achei um jeito de contornar o ultimo erro, e no finalzinho do que fiz acabei achando outro

--- um game/Scripts/AnimatedSprite.gd
+++ um game/Scripts/AnimatedSprite.gd
@@ -1,8 +1,12 @@
 extends AnimatedSprite
 
+signal hit
 # Declare member variables here. Examples:
 const SPEED : int = 400
-var screen_size
+onready var screen_size = get_viewport_rect().size
+onready var anim = $"."
+onready var collision = $"../collision"
+
 export var cocacola : float = 1
 export var jogo : bool = false
 var vida : int = 15
@@ -13,7 +17,7 @@ func _gethealth():
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	screen_size = get_viewport_rect().size
+	hide()
 	vida = 20.1
 	print(_gethealth())
 
@@ -22,5 +26,33 @@ func _process(delta):
 	var velocity = Input.get_vector("move_left","move_right","move_up","move_down")
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * SPEED * cocacola
+	
+	if velocity.x != 0:
+		anim.play("move")
+	elif velocity.y > 0:
+		anim.play("move up")
+	elif velocity.y < 0:
+		anim.play("move down")
+	else:
+		anim.play("idle")
+	
+	
+	
+	anim.flip_h = true if velocity.x > 0 else false
+	
 	position += velocity * delta
-	position = position.clamp(Vector2.ZERO, screen_size)
+	#position = position.clamp(Vector2.ZERO, screen_size)
+	
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+
+
+func _on_player_body_entered(body):
+	hide()
+	hit.emit()
+	collision.set_deferred("disabled", true)
+
+func start_pos(pos):
+	position = pos
+	show()
+	collision.disabled = false
